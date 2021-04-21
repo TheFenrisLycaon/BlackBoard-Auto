17c86b080a21148f1438f1adf67bf5e09e2d4e96
Rishabh Anand <thefenrislycaon@gmail.com>
2021-04-21 12:45:18 +0530

    Fixes

--- sysFiles/tt.sh
+++ sysFiles/tt.sh
@@ -20,30 +20,37 @@ do
     if [ $t -le 1044 -a $t -ge 959 ]
     then
         l=1
+        t=$(date +%H%M)
         limit=$(expr 1100 - $t - 40)
     elif [ $t -le 1144 -a $t -ge 1059 ]
     then 
         l=2
+        t=$(date +%H%M)
         limit=$(expr 1200 - $t - 40)
     elif [ $t -le 1244 -a $t -ge 1159 ]
     then 
         l=3
+        t=$(date +%H%M)
         limit=$( expr 1345 - $t - 40)
     elif [ $t -le 1344 -a $t -ge 1244 ]
     then 
         l=4
+        t=$(date +%H%M)
         limit=$( expr 1345 - $t - 40)
     elif [ $t -le 1430 -a $t -ge 1344 ]
     then 
         l=5
+        t=$(date +%H%M)
         limit=$( expr 1445 - $t - 40)
     elif [ $t -le 1530 -a $t -ge 1444 ]
     then 
         l=6
+        t=$(date +%H%M)
         limit=$( expr 1545 - $t )
     elif [ $t -le 1630 -a $t -ge 1544 ]
     then 
         l=7
+        t=$(date +%H%M)
         limit=$( expr 1630 - $t - 40)
     else
         echo "You're too late to handle the power of spinzitzu";exit
